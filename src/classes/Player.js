e5f8a62bddf8ff404e4532566afd4bdcfdacbc05
Lucas <lucasrafael123araujolima@gmail.com>
2024-12-21 03:50:24 -0400

    feat: :sparkles: Base do Projeto

---
+++ src/classes/Player.js
@@ -0,0 +1,73 @@
+import {
+  INITIAL_FRAMES,
+  PATH_ENGINE_IMAGE,
+  PATH_ENGINE_SPRITES,
+  PATH_SPACESHIP_IMAGE,
+} from '../utils/constants.js'
+
+class Player {
+  constructor(canvasWidth, canvasHeight) {
+    this.alive = true
+    this.width = 48 * 2
+    this.height = 48 * 2
+    this.velocity = 6
+
+    this.position = {
+      x: canvasWidth / 2 - this.width / 2,
+      y: canvasHeight - this.height - 30,
+    }
+
+    this.image = this.getImage(PATH_SPACESHIP_IMAGE)
+    this.engineImage = this.getImage(PATH_ENGINE_IMAGE)
+    this.engineSprites = this.getImage(PATH_ENGINE_SPRITES)
+
+    this.sx = 0
+    this.framesCounter = INITIAL_FRAMES
+  }
+
+  getImage(path) {
+    const image = new Image()
+    image.src = path
+    return image
+  }
+
+  moveLeft() {
+    this.position.x -= this.velocity
+  }
+
+  moveRight() {
+    this.position.x += this.velocity
+  }
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height,
+    )
+
+    ctx.drawImage(
+      this.engineSprites,
+      this.sx,
+      0,
+      48,
+      48,
+      this.position.x,
+      this.position.y + 10,
+      this.width,
+      this.height,
+    )
+
+    ctx.drawImage(
+      this.engineImage,
+      this.position.x,
+      this.position.y + 8,
+      this.width,
+      this.height,
+    )
+  }
+}
+
+export default Player
